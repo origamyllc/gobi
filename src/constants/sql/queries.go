9f40c72ca1c10058aa875d8e0eac2d0ce4070e63
Prashun Javeri <prashunjaveri@Prashuns-MBP.frontierlocal.net>
2018-10-22 13:22:09 -0400

    get JWT

--- src/constants/sql/queries.go
+++ src/constants/sql/queries.go
@@ -27,6 +27,7 @@ VALUES ('username','password', 'secret' ,'first_name', 'last_name', 'email', 'or
 
 const GET_USERS_QUERY  = `SELECT * FROM users`;
 const GET_USER_BY_ID_QUERY = "SELECT * FROM users WHERE user_id=";
+const GET_USER_BY_USER_NAME_AND_PASSWORD_QUERY = "SELECT * FROM users WHERE username = '%s' AND password = '%s' ";
 const ADD_USER_QUERY = "INSERT INTO users (username,  password, secret, first_name, last_name, email, organization_name ) VALUES ('%s','%s','%s','%s','%s','%s','%s');"
 const DELETE_USER_BY_ID_QUERY = "DELETE FROM users WHERE user_id= "
 const UPDATE_USER_NAME__QUERY =  `UPDATE users SET first_name = '%s', last_name = '%s' WHERE user_id= %d;`
--- src/controllers/authenticateUser.go
+++ src/controllers/authenticateUser.go
@@ -4,9 +4,15 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
+	statics "gobi/src/constants/app"
+	queries "gobi/src/constants/sql"
 	login_dao "gobi/src/dao/Login"
+	user_dao "gobi/src/dao/User"
     JwtToken "gobi/src/dao/Login"
+	db "gobi/src/database/postgres"
+	"log"
 	"net/http"
+	"os"
 )
 
 func AuthenticateUser(w http.ResponseWriter, r *http.Request){
@@ -28,17 +34,59 @@ func AuthenticateUser(w http.ResponseWriter, r *http.Request){
 	//      token
 
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"username": data.Username,
-		"password": data.Password,
-	});
+	conn, err := db.Connect();
+	if err != nil {
+		panic(err)
+	}
+	host, _ := os.Hostname();
+	port := statics.PORT;
+	sqlStatement := fmt.Sprintf(queries.GET_USER_BY_USER_NAME_AND_PASSWORD_QUERY, data.Username, data.Password )
+	var userDao user_dao.User;
+	var users []user_dao.User;
+	rows,err := db.Get(sqlStatement,conn)
+	for rows.Next() {
+		error :=  rows.Scan(&userDao.ID, &userDao.Username, &userDao.Password,&userDao.Secret, &userDao.FirstName, &userDao.LastName, &userDao.Email,&userDao.OrganizationName,&userDao.Created,&userDao.Updated)
+		if error != nil {
+			log.Fatal("Host: "+host +"Port: " +port)
+			log.Fatal(error)
+			panic(error)
+		} else{
+			users = append(users,userDao);
+		}
+	}
+	m,err := json.Marshal(users);
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(422) // unprocessable entity
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			panic(err)
+		}
+	}
+	log.Print("Host: "+host +"Port: " +port+ " result:= "+string(m))
+	if string(m) != "null"  {
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"username": data.Username,
+			"password": data.Password,
+		});
 
-	tokenString, error := token.SignedString([]byte("secret"))
-	if error != nil {
-		fmt.Println(error)
+		tokenString, error := token.SignedString([]byte("secret"))
+		if error != nil {
+			fmt.Println(error)
+		}
+
+		json.NewEncoder(w).Encode(JwtToken.JwtToken{Token: tokenString})
+	} else {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(500) // unprocessable entity
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			panic(err)
+		}
 	}
 
-	json.NewEncoder(w).Encode(JwtToken.JwtToken{Token: tokenString})
+	defer rows.Close()
+	defer conn.Close();
+
+
 
 
 }

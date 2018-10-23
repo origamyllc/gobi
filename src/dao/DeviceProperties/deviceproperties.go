46d6da2c583121e3334647782aa519d329840267
prashunjaveri <prashunjaveri@gmail.com>
2018-10-23 00:56:53 -0400

    adding models sql

---
+++ src/dao/DeviceProperties/deviceproperties.go
@@ -0,0 +1,13 @@
+package DeviceProperties
+
+/**
+const deviceProperties = [
+'object-identifier',
+'object-name', 'description', 'system-status', 'vendor-name',
+'vendor-identifier', 'model-name', 'firmware-revision', 'application-software-version', 'location', 'local-time', 'local-date',
+'utc-offset', 'daylight-savings-status', 'protocol-version', 'protocol-revision',
+'protocol-services-supported', 'object-types-supported' 96, 'object-list', 'max-apdu-length-accepted',
+'segmentation-supported', 'apdu-timeout', 'number-of-apdu-retries',  'device-address-binding', 'database-revision',
+'max-info-frames', 'max-master' // , 'active-cov-subscriptions'
+]
+**/
